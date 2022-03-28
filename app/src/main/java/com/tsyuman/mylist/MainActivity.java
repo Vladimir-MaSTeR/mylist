c97bfa1c49637d693bd2e6f208b747c52309b977
Цюман Владимир Владимирович <9e4cA5GUD3>
2022-03-29 00:18:13 +0700

    Initial commit

---
+++ app/src/main/java/com/tsyuman/mylist/MainActivity.java
@@ -0,0 +1,14 @@
+package com.tsyuman.mylist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
